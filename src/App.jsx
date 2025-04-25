cba43fd0718c5db81dee126474dba2aa50fed00e
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-04-26 00:16:54 +0530

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar"
+import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 const App=()=>{
@@ -14,6 +15,7 @@ const App=()=>{
           <Navbar />
           <Hero />
           <Technologies />
+          <Projects />
         </div>
          </div>
   )

55f6d60a3950843d45a5245b343034eadc79dba9
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-04-25 23:01:12 +0530

    added hero section and debugged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             <a href="/" aria-label="Home">
             <img src={logo}  className="mx-2" width={50} height={33} alt="Logo" /></a>
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2x1">
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href="https://www.linkedin.com/in/rishabh-pradhan-59498a28a/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
             <a href="https://github.com/Rishi2564" target="_blank" rel="noopener noreferrer" aria-label="Github"><FaGithub /></a>
             <a href="https://www.linkedin.com/in/rishabh-pradhan-59498a28a/" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram /></a>

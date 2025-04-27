1bf1e56b8073b0d3814cfd304057fb458807c39f
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-04-27 19:10:56 +0530

    changed the links of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href="https://www.linkedin.com/in/rishabh-pradhan-59498a28a/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
             <a href="https://github.com/Rishi2564" target="_blank" rel="noopener noreferrer" aria-label="Github"><FaGithub /></a>
-            <a href="https://www.linkedin.com/in/rishabh-pradhan-59498a28a/" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram /></a>
-            <a href="https://www.linkedin.com/in/rishabh-pradhan-59498a28a/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FaSquareXTwitter /></a>
+            <a href="https://www.instagram.com/realrishabhpradhan/?next=%2F" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><FaInstagram /></a>
+            <a href="https://x.com/RISHABHPRA18574" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FaSquareXTwitter /></a>
         </div>
     </nav>
   )
